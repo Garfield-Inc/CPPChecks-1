12f4de1e25a026cb72164763397ec3b2192c544d
Soumya Satish <88482100+soumyaEmb@users.noreply.github.com>
2021-08-17 22:32:58 +0530

    Add files via upload

---
+++ MoveConst.cpp
@@ -0,0 +1,17 @@
+class X{
+    /* custom copy and move constructors defined */
+};
+void foo_vulnerable(){
+    const X bar;
+    X quux = std::move(bar); //copy constructor is silently called here instead of expected move constructor
+}
+
+void foo_fix1(){
+    X bar;										//Remove const
+    X quux{std::move(bar)};                     //Compliant
+}
+
+void foo_fix2(){
+    const X bar;
+    X quux{bar};								//Remove std::move() --Compliant
+}
